0af071787ffe8dc3e60574d613ec8a85521f2111
nishimura <nishimura@sample.com>
2026-06-20 19:11:51 +0900

    fix(App.WinForms.HostDI): DapperルートにSqlMapperの型ハンドラー登録を追加
    DapperRepo.ProductRepositoryに切り替えるとSQLiteの動的型付け（Id列の
    BLOB/TEXT混在等）でInvalidCastExceptionになるため、ManualDI／DIContainer
    と同様にGuidTypeHandler／DecimalTypeHandlerをHost構築前に登録した。
    AdoNet／Dapper／EF Coreの3ルートが排他的に切り替えられることをコメントで
    明示し、AppDbContextの登録をEF Core用のコメント群と並べて分かりやすくした。

--- CSBestpPactice/App.WinForms.HostDI/Program.cs
+++ CSBestpPactice/App.WinForms.HostDI/Program.cs
@@ -1,15 +1,16 @@
 using CSBestpPactice.Domain.Repositories;
 using CSBestpPactice.Infrastructure.Data.Factories;
 using CSBestpPactice.Infrastructure.Data.Sessions;
-using AdoNet = CSBestpPactice.Infrastructure.Repositories.AdoNet;
-using DapperRepo = CSBestpPactice.Infrastructure.Repositories.Dapper;
-using EfCore = CSBestpPactice.Infrastructure.Repositories.EfCore;
+using CSBestpPactice.Infrastructure.Repositories.AdoNet;
 using CSBestpPactice.Service;
+using Dapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CSBestpPactice.Infrastructure.Repositories.AdoNet;
+using AdoNet = CSBestpPactice.Infrastructure.Repositories.AdoNet;
+using DapperRepo = CSBestpPactice.Infrastructure.Repositories.Dapper;
+using EfCore = CSBestpPactice.Infrastructure.Repositories.EfCore;
 
 namespace App.WinForms.HostDI
 {
@@ -25,22 +26,28 @@ namespace App.WinForms.HostDI
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
+            SqlMapper.AddTypeHandler(new DapperRepo.GuidTypeHandler());
+            SqlMapper.AddTypeHandler(new DapperRepo.DecimalTypeHandler());
+
             using IHost host = Host.CreateDefaultBuilder()
                 .UseContentRoot(AppContext.BaseDirectory)
                 .ConfigureServices((context, services) =>
                 {
                     var connectionString = context.Configuration.GetConnectionString("SQLite")!;
 
-                    services.AddDbContext<EfCore.AppDbContext>(
-                        options => options.UseSqlite(connectionString),
-                        ServiceLifetime.Singleton);
-
                     services.AddSingleton<IDbConnectionFactory>(_ =>
                         new SqliteConnectionFactory(connectionString));
                     services.AddSingleton<IDbSession>(sp =>
                         new DbSession(sp.GetRequiredService<IDbConnectionFactory>().CreateConnection()));
 
+                    // Ado.NET, Dapper, EF Core repository registrations are mutually exclusive, choose one of them to register
+                    //services.AddTransient<IProductRepository, AdoNet.ProductRepository>();
+                    //services.AddTransient<IProductRepository, DapperRepo.ProductRepository>();
+                    services.AddDbContext<EfCore.AppDbContext>(
+                        options => options.UseSqlite(connectionString),
+                        ServiceLifetime.Singleton);
                     services.AddTransient<IProductRepository, EfCore.ProductRepository>();
+
                     services.AddTransient<IProductTableRepository, ProductTableRepository>();
                     services.AddTransient<IProductService, ProductService>();
                     services.AddTransient<IProductTableService, ProductTableService>();
